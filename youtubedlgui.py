03aafab3856c97067434b28332e9b9b26e9296fb
josev814 <josev814@gmail.com>
2020-03-01 13:06:12 -0500

    updating to python 3
    allowing to open the savedir
    making the save dir create a new folder for the current day

--- youtubedlgui.py
+++ youtubedlgui.py
@@ -3,8 +3,12 @@ from sys import exit
 import subprocess
 import re
 import requests
-from Tkinter import *
+import time
+#from Tkinter import *
 from tkinter import messagebox
+from tkinter import Frame,Label,BOTTOM,StringVar,Entry,Button,Tk,LEFT,OptionMenu
+import datetime
+
 
 APP = ''
 FRAME = ''
@@ -12,36 +16,62 @@ APPW = 720
 APPH = 405
 SCREENW = SCREENH = APPX = APPY = None
 DEBUG = True
+today = datetime.datetime.now().date()
 homedir = os.path.expanduser("~")
 dldir = os.path.join(homedir, "Downloads")
+savedir = os.path.join(homedir, "Downloads\{}-{}-{}".format(today.year, today.month, today.day))
 ytdl = os.path.join(dldir,"youtube-dl.exe")
 ytdl2 = os.path.join(dldir,"youtube-dl")
+youtube_dl_url = 'https://youtube-dl.org/downloads/latest/youtube-dl.exe'
 vidRegex = re.compile(r'^(?P<number>[0-9]+)[\s\t]+(?P<format>3gp|3gp|webm|mp4)[\s\t]+(?P<resolution>[0-9x]+)[\s\t]+(?P<info>.*)',re.IGNORECASE)
-ignoreVidRegex = re.compile(r'video only',re.IGNORECASE)
+ignoreVidRegex = re.compile(r'video only', re.IGNORECASE)
 VIDLIST={}
 YTLINK=''
 
-if os.path.exists(dldir):
-    os.chdir(dldir)
-else:
-    messagebox.showerror(
+
+def display_message(title='Youtuble-dl', message='', mtype='info'):
+    if mtype == 'error':
+        messagebox.showerror(message=message)
+    elif mtype == 'warning':
+        messagebox.showwarning(message=message)
+    else:
+        messagebox.showinfo(message=message)
+
+
+if os.path.exists(dldir) is False:
+    display_message(
         "Missing Downloads Directory",
-        "We couldn't find your Downloads directory. " + dldir +
-        " Verify that it exists before opening this program again."
+        "We couldn't find your Downloads directory ({}). Verify that it exists before opening this program again.".format(dldir),
+        'error'
     )
     exit()
+
+if os.path.exists(savedir) is False:
+    os.mkdir(savedir)
+
+if os.path.exists(savedir):
+    os.chdir(savedir)
+else:
+    display_message(
+        "Missing Downloads Directory",
+        "We couldn't create the directory to download into ({}). Verify that it exists before opening this program again.".format(savedir),
+        'error'
+    )
+    exit()
+
 
 if os.path.exists(ytdl):
     ytdl = ytdl
 elif os.path.exists(ytdl2):
     ytdl = ytdl2
 else:
-    print(
+    display_message(
         "Missing youtube-dl.exe",
         "We couldn't find youtube-dl.exe in your Downloads directory. " +
-        "We will attempt to download it now. This will take a minute."
+        "We will attempt to download it now. This will take a minute.",
+        'info'
     )
-    r = requests.get('https://youtube-dl.org/downloads/latest/youtube-dl.exe', allow_redirects=True, stream=True)
+    r = requests.get(youtube_dl_url, allow_redirects=True, stream=True)
     with open('youtube-dl.exe', 'wb') as ytfile:
         for chunk in r.iter_content(chunk_size=1024):
             if chunk:
@@ -49,47 +79,68 @@ else:
     if os.path.exists(ytdl):
         ytdl = ytdl
     else:
-        messagebox.showerror(
+        display_message(
             "Missing youtube-dl.exe",
             "We couldn't find youtube-dl.exe in your Downloads directory. " +
-            "Download it using this link https://youtube-dl.org/downloads/latest/youtube-dl.exe" +
+            "Download it using this link {}" +
             "Make sure it's in your Downloads directory. " +
-            "Then restart this program."
+            "Then restart this program.".format(youtube_dl_url),
+            'error'
         )
         exit()
 
 #upgrade youtube-dl
-print('Checking for youtube-dl updates')
-proc = subprocess.Popen([ytdl,"-U"],stdout=subprocess.PIPE)
+display_message('Youtube-dl Updating', 'Checking for youtube-dl updates, press OK to proceed')
+
+proc = subprocess.Popen(
+    [
+        ytdl,
+        "-U"
+    ],
+    stdout=subprocess.PIPE
+)
 while True:
-    line = proc.stdout.readline()
-    if line != '':
+    line = proc.stdout.readline().decode()
+    if line != '' and DEBUG:
         print(line)
     else:
         break
 
 def getVideoList():
     global VIDLIST
-    proc = subprocess.Popen([ytdl,"-F",YTLINK],stdout=subprocess.PIPE)
+    proc = subprocess.Popen(
+        [
+            ytdl,
+            "-F",
+            YTLINK
+        ],
+        stdout=subprocess.PIPE
+    )
     while True:
-        line = proc.stdout.readline()
+        line = proc.stdout.readline().decode()
         if line != '':
             #print "current line:", line.rstrip()
-            videoMatch = re.match(vidRegex,line.rstrip())
-            if(videoMatch):
-                ignore = re.search(ignoreVidRegex,line.rstrip())
-                if( ignore is None):
-                    VIDLIST[videoMatch.group('format') +
-                        ' ' + videoMatch.group('resolution') +
-                        ' ' + videoMatch.group('info')] = {
-                        videoMatch.group('number')
-                    }
+            videoMatch = re.match(
+                vidRegex,
+                line.rstrip()
+            )
+            if videoMatch:
+                ignore = re.search(ignoreVidRegex, line.rstrip())
+                if ignore is None:
+                    videoName = '{} {} {}'.format(
+                        videoMatch.group('format'),
+                        videoMatch.group('resolution'),
+                        videoMatch.group('info')
+                    )
+                    VIDLIST[videoName] = '{}'.format(videoMatch.group('number'))
         else:
             break
+
     if not VIDLIST: #empty dictionary
-        messagebox.showerror(
+        display_message(
             "Unable to Download",
-            "We are unable to download that specific YouTube video."
+            "We are unable to download that specific YouTube video.",
+            'error'
         )
 
 def set_app_position():
@@ -107,8 +158,15 @@ def set_app_position():
     APP.geometry('%dx%d+%d+%d' % (APPW, APPH, APPX, APPY))
 
 def openFileLocation():
-    subprocess.Popen('explorer ' + dldir)
-    return
+    opendir = messagebox.askyesno('Downloaded File', 'Would you like to open the download directory?')
+    print(opendir)
+    if opendir:
+        print('opening {}'.format(savedir))
+        subprocess.call([
+            'explorer.exe',
+            savedir
+        ])
+    exit()
 
 def downloadFile(VIDLIST, option):
     FRAME = Frame(APP, width=APPW, height=APPH)
@@ -121,17 +179,16 @@ def downloadFile(VIDLIST, option):
     FRAME.pack()
     FRAME.tkraise()
 
-    for video,key in VIDLIST.iteritems():
+    for video, key in VIDLIST.items():
         if video == option:
             proc = subprocess.Popen([ytdl, "-f", key, YTLINK], stdout=subprocess.PIPE)
             while True:
-                line = proc.stdout.readline()
-                if line != '':
+                line = proc.stdout.readline().decode()
+                if line != '' and DEBUG:
                     print(line)
                 else:
                     break
     openFileLocation()
-    exit()
 
 def open_app():
     global APP
